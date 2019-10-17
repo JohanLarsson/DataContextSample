a6356d2efee87dc657ed02c8654e461e35ee56a8
Johan Larsson <johan_gavle1@hotmail.com>
2019-10-17 18:09:48 +0200

    Initial.

---
+++ DataContextSample/B.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DataContextSample
+{
+    /// <summary>
+    /// Interaction logic for B.xaml
+    /// </summary>
+    public partial class B : UserControl
+    {
+        public B()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataContextSample/ViewModel.cs
@@ -0,0 +1,52 @@
+﻿namespace DataContextSample
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+
+    public class ViewModel : INotifyPropertyChanged
+    {
+        private int left;
+        private int right;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int Sum => this.Left + this.Right;
+
+        public int Left
+        {
+            get => this.left;
+            set
+            {
+                if (value == this.left)
+                {
+                    return;
+                }
+
+                this.left = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Sum));
+            }
+        }
+
+        public int Right
+        {
+            get => this.right;
+            set
+            {
+                if (value == this.right)
+                {
+                    return;
+                }
+
+                this.right = value;
+                this.OnPropertyChanged();
+                this.OnPropertyChanged(nameof(this.Sum));
+            }
+        }
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
